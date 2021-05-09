61d0dea4c55bebe9495d38a227418eaa22ae48d1
AhmedSaad <krarooo281@gmail.com>
2021-05-09 17:10:58 +0200

    Add files via upload

---
+++ app/src/mmain/java/com/horaspolice/materialabout/views/ViewIdGenerator.java
@@ -0,0 +1,22 @@
+package com.horaspolice.materialabout.views;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.view.View;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by jrvansuita on 10/03/17.
+ */
+
+public class ViewIdGenerator {
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    @SuppressLint("NewApi")
+    public static int generateViewId() {
+
+        return View.generateViewId();
+
+    }
+}
