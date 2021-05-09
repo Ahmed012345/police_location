a5efd2224d4b6554cf2ee24f381189d562224d66
AhmedSaad <krarooo281@gmail.com>
2021-05-09 17:09:55 +0200

    Add files via upload

---
+++ app/src/mmain/java/com/horaspolice/materialabout/util/VisibleUtil.java
@@ -0,0 +1,15 @@
+package com.horaspolice.materialabout.util;
+
+import androidx.annotation.Nullable;
+import android.view.View;
+
+/**
+ * Created by jrvansuita on 20/02/17.
+ */
+
+public final class VisibleUtil {
+
+    public static void handle(View v, @Nullable String s) {
+        v.setVisibility(s == null || s.isEmpty() ? View.GONE : View.VISIBLE);
+    }
+}
